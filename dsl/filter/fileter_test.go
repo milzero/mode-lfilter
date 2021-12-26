6dc2d9e0d850f45e5cd604cc179cd689bf7948dc
milzero <yilelu@hotmail.com>
2021-12-27 00:45:59 +0800

    update filter files

--- dsl/filter/fileter_test.go
+++ dsl/filter/fileter_test.go
@@ -1,11 +1,33 @@
 package filter
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
 	"testing"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 func TestName(t *testing.T) {
-	fmt.Println("read config")
+	conf := new(module.Yaml)
+	yamlFile, err := ioutil.ReadFile("test.yaml")
+
+	// conf := new(module.Yaml1)
+	// yamlFile, err := ioutil.ReadFile("test.yaml")
+
+	// conf := new(module.Yaml2)
+	//  yamlFile, err := ioutil.ReadFile("test1.yaml")
+
+	log.Println("yamlFile:", yamlFile)
+	if err != nil {
+		log.Printf("yamlFile.Get err #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, conf)
+	// err = yaml.Unmarshal(yamlFile, &resultMap)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	log.Println("conf", conf)
+	// log.Println("conf", resultMap)
 
 }
