00f321ca932fbad3efd3eddc58e54376ce54d55e
milzero <yilelu@hotmail.com>
2021-12-28 00:55:00 +0800

    add filter container

--- dsl/filter/fileter.go
+++ dsl/filter/fileter.go
@@ -1,4 +1,28 @@
 package filter
 
+type ModelContainer struct {
+	FilterModel Model `yaml:"filterModel"`
+}
+type Item struct {
+	Key   string `yaml:"key"`
+	Value string `yaml:"value"`
+}
+type Items struct {
+	Item Item `yaml:"item"`
+}
 type Filter struct {
+	Desc     string  `yaml:"desc"`
+	Function string  `yaml:"function"`
+	Type     string  `yaml:"type"`
+	Method   string  `yaml:"methon"`
+	Items    []Items `yaml:"items"`
+}
+type Filters struct {
+	Filter Filter `yaml:"filte"`
+}
+type Model struct {
+	Version   string    `yaml:"version"`
+	Namespace string    `yaml:"namespace"`
+	Desc      string    `yaml:"desc"`
+	Filters   []Filters `yaml:"filtes"`
 }
